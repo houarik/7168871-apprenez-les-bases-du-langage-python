cde695627cb047ac942285437b2a0e72a509fd06
houarik <houarikarim@yahoo.fr>
2025-03-09 21:45:01 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,9 @@
 # Écrivez votre code ici !
+fruits=["pomme","banane","orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits.remove(fruits[1])
+fruits.insert(1,"ananas")
+print(len(fruits))
+fruits.sort()
+print(fruits)
